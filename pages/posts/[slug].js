c2b45f8f074f1f704d511ca6592b991fba130bea
dastasoft <dastasoft@protonmail.com>
2021-02-09 23:17:38 +0100

    fixed slug

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,50 +1,54 @@
-import { useRouter } from "next/router";
-import ErrorPage from "next/error";
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
 
-import { getPostBySlug, getAllPosts } from "../../lib/api";
-import markdownToHtml from "../../lib/markdownToHtml";
+import { getPostBySlug, getAllPosts } from '../../lib/api'
+import markdownToHtml from '../../lib/markdownToHtml'
 
 export default function Post({ post }) {
-  const router = useRouter();
+  const router = useRouter()
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />;
+    return <ErrorPage statusCode={404} />
   }
-  return <div>{post.title}</div>;
+  return (
+    <div>
+      <h1>{post.title}</h1>
+    </div>
+  )
 }
 
 export async function getStaticProps({ params }) {
   const post = getPostBySlug(params.slug, [
-    "title",
-    "date",
-    "slug",
-    "author",
-    "content",
-    "ogImage",
-    "coverImage"
-  ]);
-  const content = await markdownToHtml(post.content || "");
+    'title',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+  const content = await markdownToHtml(post.content || '')
 
   return {
     props: {
       post: {
         ...post,
-        content
-      }
-    }
-  };
+        content,
+      },
+    },
+  }
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(["slug"]);
+  const posts = getAllPosts(['slug'])
 
   return {
     paths: posts.map(post => {
       return {
         params: {
-          slug: post.slug
-        }
-      };
+          slug: post.slug,
+        },
+      }
     }),
-    fallback: false
-  };
+    fallback: false,
+  }
 }
