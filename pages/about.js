5c117d96d87ec7e8117f215e252282425716cc9c
dastasoft <dastasoft@protonmail.com>
2021-02-18 20:20:27 +0100

    fixed export on page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
 import Image from 'next/image'
+import styled from 'styled-components'
 
+import {
+  colorBackgroundAndTransition,
+  imageDarkModeSupport,
+} from '@/components/GlobalStyle'
 import { ICON_SIZES, URLs } from '@/lib/constants'
-
-import { Container, Title, Text, SubTitle, IconHolder } from './About.styles'
 
 const About = () => {
   return (
@@ -110,5 +113,41 @@ const About = () => {
     </Container>
   )
 }
+
+export const Container = styled.div`
+  padding: 1rem;
+  font-size: 18px;
+  ${({ theme }) => colorBackgroundAndTransition(theme)}
+`
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 1rem;
+`
+
+export const Text = styled.div`
+  p {
+    margin: 1rem 0 2rem 0;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 24px;
+  }
+`
+
+export const SubTitle = styled.div`
+  text-align: center;
+  font-weight: 500;
+  margin-bottom: 1rem;
+`
+
+export const IconHolder = styled.div`
+  ${({ theme }) => imageDarkModeSupport(theme.contentText)}
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 export default About
