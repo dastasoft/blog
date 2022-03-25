97d1ac3c699895db5f6079275120b72a1540c076
dastasoft <dastasoft@protonmail.com>
2022-03-25 12:51:07 +0100

    added rss link

--- components/Menu/SocialNetworks/index.js
+++ components/Menu/SocialNetworks/index.js
@@ -1,23 +1,34 @@
-import { FaGithub, FaLinkedin, FaDev, FaTwitter } from 'react-icons/fa'
+import {
+  FaGithub,
+  FaLinkedin,
+  FaDev,
+  FaTwitter,
+  FaRssSquare,
+} from 'react-icons/fa'
 
 import { URLs } from '@/lib/constants'
 
 import { IconHolder } from './SocialNetworks.styles'
 
+const ICON_FONT_SIZE = 32
+
 const SocialNetworks = () => {
   return (
     <IconHolder>
       <a href={URLs.GITHUB} target="_blank" rel="noopener noreferrer">
-        <FaGithub fontSize="32px" />
+        <FaGithub fontSize={ICON_FONT_SIZE} />
       </a>
       <a href={URLs.DEV} target="_blank" rel="noopener noreferrer">
-        <FaDev fontSize="32" />
+        <FaDev fontSize={ICON_FONT_SIZE} />
       </a>
       <a href={URLs.LINKEDIN} target="_blank" rel="noopener noreferrer">
-        <FaLinkedin fontSize="32" />
+        <FaLinkedin fontSize={ICON_FONT_SIZE} />
       </a>
       <a href={URLs.TWITTER} target="_blank" rel="noopener noreferrer">
-        <FaTwitter fontSize="32px" />
+        <FaTwitter fontSize={ICON_FONT_SIZE} />
+      </a>
+      <a href={URLs.RSS} target="_blank" rel="noopener noreferrer">
+        <FaRssSquare fontSize={ICON_FONT_SIZE} />
       </a>
     </IconHolder>
   )
